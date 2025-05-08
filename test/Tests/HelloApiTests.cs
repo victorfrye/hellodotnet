59bdd166cd0d1a5c38fc166feab64bb7877aa2ae
Victor Frye <victorfrye@outlook.com>
2025-05-07 21:23:09 -0400

    feat: add test server tests

---
+++ test/Tests/HelloApiTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace VictorFrye.HelloDotnet.Tests;
+
+public class HelloApiTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+{
+    [Fact]
+    public async Task GetHelloReturnsHelloDotnet()
+    {
+        var client = factory.CreateClient();
+
+        var response = await client.GetAsync("/hello");
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.Equal("Hello, .NET!", content);
+    }
+}
